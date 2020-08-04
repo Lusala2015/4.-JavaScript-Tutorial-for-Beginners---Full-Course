8e625cafcebc7c597bd84704c13abe070833c005
MarkLusalaMudoga <dev.mmlusala@gmail.com>
2020-08-04 08:22:03 +0300

    2nd way of including javasript code

---
+++ demo.js
@@ -0,0 +1,13 @@
+document.write("<h1>Hello World</h1>");
+        document.write("<h2>Hello World</h2>");
+        document.write("<h3>Hello World</h3>");
+        document.write("<h4>Hello World</h4>");
+        document.write("<h5>Hello World</h5>");
+        document.write("<h6>Hello World</h6>");
+
+        document.write("<h1>Hello World</h1>");
+        document.write("<h2>Hello World</h2>");
+        document.write("<h3>Hello World</h3>");
+        document.write("<h4>Hello World</h4>");
+        document.write("<h5>Hello World</h5>");
+        document.write("<h6>Hello World</h6>"); 
