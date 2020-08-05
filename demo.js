86187723009f8d3ae4f939e0edc1eb34e3805c4c
MarkLusalaMudoga <dev.mmlusala@gmail.com>
2020-08-05 11:29:46 +0300

    switch control statement

--- demo.js
+++ demo.js
@@ -4,5 +4,30 @@ document.write("<h1>Hello World</h1>");
         document.write("<h4>Hello World</h4>");
         document.write("<h5>Hello World</h5>");
         document.write("<h6>Hello World</h6>");
-
+var day = 3;
+switch(day){
+        case 1:
+                document.write('Sunday');
+                break;
+        case 2:
+                document.write('Monsday');
+                break;
+        case 3:
+                document.write('Tuesday');
+                break;
+        case 4:
+                document.write('Wenesday');
+                break;
+        case 4:
+                document.write('Thursday');
+                break;
+        case 5:
+                document.write('Friday');
+                break;
+        case 6:
+                document.write('Saturday')
+                break;
+        default:
+                document.write('Wrong Input');
+}
         
